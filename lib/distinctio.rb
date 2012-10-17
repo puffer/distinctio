53d155630d424fb373addc24032134c2ab837278
Andrew Gridnev <andrew.gridnev@gmail.com>
2012-10-17 17:08:28 +0800

    fixed falling specs

--- lib/distinctio.rb
+++ lib/distinctio.rb
@@ -1,7 +1,6 @@
 require 'diff_match_patch'
 require 'active_support/all'
 require 'distinctio/version'
-require 'distinctio/distinctio'
 require 'distinctio/differs/base'
 require 'distinctio/differs/object'
 require 'distinctio/differs/simple'
